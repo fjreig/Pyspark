6d375e95de78d05175d18b6df2771f120e4f0210
FJ Reig <48714328+fjreig@users.noreply.github.com>
2024-07-16 13:09:55 +0200

    Create Pipeline.py

---
+++ src/Pipeline.py
@@ -0,0 +1,57 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import logging
+import os
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s:%(funcName)s:%(levelname)s:%(message)s"
+)
+
+def create_spark_session() -> SparkSession:
+    spark = (
+        SparkSession.builder.appName("Pipeline PostgreSQL to Kafka")
+        .config(
+            "spark.jars.packages",
+            "org.postgresql:postgresql:42.7.2,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1",
+        )
+        .getOrCreate()
+    )
+    logging.info("Spark session created successfully")
+    return (spark)
+
+def create_initial_dataframe(spark_session):
+    try:
+        query = """SELECT * FROM public.fv_apv_emi """
+        df = spark_session.read \
+            .format("jdbc") \
+            .option("url", os.environ['url_postgres']) \
+            .option("user", os.environ['user_postgres']) \
+            .option("password", os.environ['password_postgres']) \
+            .option("query", query) \
+            .load()
+        logging.info("Initial dataframe created successfully")
+    except Exception as e:
+        logging.warning(f"Initial dataframe couldn't be created due to exception: {e}")
+        raise
+    return (df)
+
+def create_final_dataframe(df):
+    df = df.select(to_json(struct("Fecha")).alias("key"), to_json(struct("Fecha","radiacion","temp_amb","temp_panel")).alias("value"))
+    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+        .write \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", os.environ['kafka_broker']) \
+        .option("topic", "emi") \
+        .save()
+    return (df)
+
+def main():
+    spark = create_spark_session()
+    df = create_initial_dataframe(spark)
+    df.show()
+    df_final = create_final_dataframe(df)
+    df_final.show()
+
+if __name__ == "__main__":
+    main()
